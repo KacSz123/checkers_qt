475f81d860ebd49fcca0762f07f7136ca8a6e8d4
Kacper Szczepanowski <kacperszcz159@gmail.com>
2024-02-17 15:15:18 +0100

    piece drawing, piece activated

--- inc/board_widget.hh
+++ inc/board_widget.hh
@@ -9,16 +9,25 @@
 #include <QPainter>
 #include<QPaintEvent>
 #include<QPoint>
+#include<QPoint>
+#include "checkersboard.hh"
 class BoardWidget : public QWidget
 {
+    static constexpr float boardRatioX = 0.066, boardRatioY = 0.068, squareRatioX  = 0.109, squareRatioY  = 0.10865;
+
     Q_OBJECT
     const QImage *tlo= new QImage(":/board_img/board_png");
-    const QImage *piece= new QImage(":/board_img/board_png");
+    QImage *piece= new QImage(":/board_img/piece_png");
     QTimer *_boardTimer;
     QPoint *_p;
+    CheckersBoard _board;
+
+//    QPoint computePosition(int pose){ return QPoint(int(tlo->width()*boardRatioX)pose,int(tlo->height()*boardRatioY))}
 public:
     explicit BoardWidget(QWidget *parent = nullptr);
     void paintEvent(QPaintEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    void mousePressEvent(QMouseEvent *event);
 signals:
 
 public slots:
--- src/board_widget.cpp
+++ src/board_widget.cpp
@@ -20,9 +20,17 @@ void BoardWidget::paintEvent(QPaintEvent *)
 //    qInfo()<<height()<<"    "<<width();
     const double _ScaleY=height()/static_cast<double>(tlo->height());
     const double _ScaleX=width()/static_cast<double>(tlo->width());
+
+
     p2.scale(_ScaleX,_ScaleY);
     p2.drawRect(0,0,width()+1, height()+1);
     p2.drawImage(0,0, *tlo);
+//    piece->
+//    p2.scale(_ScaleX/1.7,_ScaleY/1.7)
+
+    QImage draw = piece->scaled(int(tlo->width()*squareRatioX),int(tlo->height()*squareRatioY));
+//    qInfo()<<"squareX"<<int(width()*0.1)<<"squareY"<<int(height()*0.1);
+    p2.drawImage(int((tlo->width()*boardRatioX)+4*tlo->width()*squareRatioX),int(tlo->height()*boardRatioY+3*tlo->width()*squareRatioY), draw);
     update();
 }
 
@@ -32,3 +40,17 @@ void BoardWidget::timerEventHandler()
     qDebug()<<*_p<<" on screen: "<<height()<<"    "<<width();
 }
 
+void BoardWidget::mousePressEvent(QMouseEvent *event)
+{
+    qInfo()<<"mouse clicked! on: "<<*_p;
+    delete piece;
+    piece = new QImage(":/board_img/piece_png_act");
+
+}
+
+void BoardWidget::mouseReleaseEvent(QMouseEvent *event)
+{
+     delete piece;
+   piece = new QImage(":/board_img/piece_png");
+//    qInfo()<<"mouse clicked!";
+}
